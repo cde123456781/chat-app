2deed8105f4f728c05d8610b4a9214cbe27eef7d
cde123456781 <bjc199@uclive.ac.nz>
2025-10-10 15:55:20 +1300

    Add Error Handling for Error status codes

--- chat_app/Program.cs
+++ chat_app/Program.cs
@@ -23,14 +23,16 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseMigrationsEndPoint();
+    app.UseMigrationsEndPoint();    
 }
 
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
-app.UseAntiforgery();
 
+app.UseStatusCodePagesWithReExecute("/Error/{0}");
+
+app.UseAntiforgery();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
